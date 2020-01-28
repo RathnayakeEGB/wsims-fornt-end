358a098f332e89efb53d9496a05cb550820705a0
rathanayakaegb <rathnayakaegb@gmail.com>
2020-01-28 21:57:22 +0530

    2020-01-27

--- src/app/pages/customers-mgt/customer-group-page/customer-group-page.component.ts
+++ src/app/pages/customers-mgt/customer-group-page/customer-group-page.component.ts
@@ -15,25 +15,25 @@ import { NzNotificationService } from 'ng-zorro-antd';
 })
 export class CustomerGroupPageComponent implements OnInit {
 
-  customerGroupFb :FormGroup;
-  saveButtonText='Save';
-  getAllCusGroup=[];
+  customerGroupFb : FormGroup;
+  saveButtonText = 'Save';
+  getAllCusGroup = [];
 
-  constructor(private router:Router,private fb:FormBuilder,private http:HttpClient,private notification: NzNotificationService) { }
+  constructor(private router: Router, private fb: FormBuilder, private http: HttpClient, private notification: NzNotificationService) { }
 
   ngOnInit() {
 
-    this.customerGroupFb =this.fb.group({
-      customerGroupSeq:[null],
-      customerGroupNo:[null,[Validators.required,Validators.maxLength(20)]],
-      customerGroupName:[null,[Validators.required,Validators.maxLength(30)]],
-      description:[null,[Validators.required,Validators.maxLength(100)]],
-      isActive:[null,[Validators.required,Validators.maxLength(1)]],
-      createdBy:[null]
+    this.customerGroupFb = this.fb.group({
+      customerGroupSeq: [null],
+      customerGroupNo: [null, [Validators.required, Validators.maxLength(20)]],
+      customerGroupName: [null, [Validators.required, Validators.maxLength(30)]],
+      description: [null, [Validators.required, Validators.maxLength(100)]],
+      isActive: [null, [Validators.required, Validators.maxLength(1)]],
+      createdBy: [null]
 
     });
     this.loadGroupNo();
-    this.saveButtonText='Save';
+    this.saveButtonText = 'Save';
     this.getAllCustomerGroups();
   }
 
@@ -49,8 +49,8 @@ export class CustomerGroupPageComponent implements OnInit {
   private loadGroupNo(){
 
 
-    this.http.get<ResponsObject>(environment.apiUrl + 'numbers/getNumberById/'+UniqueNumber.CUSTOMERGROUP).subscribe((res)=>{
-      console.log(' xxxx---> ',res);
+    this.http.get<ResponsObject>(environment.apiUrl + 'numbers/getNumberById/' + UniqueNumber.CUSTOMERGROUP).subscribe((res) => {
+      console.log(' xxxx---> ', res);
       this.customerGroupFb.controls.customerGroupNo.setValue(res.status);
    });
 
@@ -61,22 +61,22 @@ export class CustomerGroupPageComponent implements OnInit {
 
     this.customerGroupFb.controls.createdBy.setValue(sessionStorage.getItem('currentUser'));
 
-    if(this.saveButtonText==='Save'){
+    if (this.saveButtonText === 'Save'){
 
       // tslint:disable-next-line: max-line-length
-      this.http.post<ResponsObject>(`${environment.apiUrl}cus_group_mgt/createNewCustomerGroup`,this.customerGroupFb.value).subscribe((res)=>{
+      this.http.post<ResponsObject>(`${environment.apiUrl}cus_group_mgt/createNewCustomerGroup`, this.customerGroupFb.value).subscribe((res) => {
         console.log(res);
 
         if ( res.statusCode === 400) {
 
-          this.notification.create('error','*** New Customer Group ***',res.status);
+          this.notification.create('error', '*** New Customer Group ***', res.status);
 
         } else if ( res.statusCode === 500) {
 
-          this.notification.create('error','*** New Customer Group ***',res.status);
+          this.notification.create('error', '*** New Customer Group ***', res.status);
 
         } else if ( res.statusCode === 200) {
-          this.notification.create('success','*** New Customer Group ***','Sucessfully Created New Customer Group !');
+          this.notification.create('success', '*** New Customer Group ***', 'Sucessfully Created New Customer Group !');
           this.clear();
         }
 
@@ -87,19 +87,19 @@ export class CustomerGroupPageComponent implements OnInit {
       this.customerGroupFb.controls.createdBy.setValue(sessionStorage.getItem('currentUser'));
 
       // tslint:disable-next-line: max-line-length
-        this.http.put<ResponsObject>(`${environment.apiUrl}cus_group_mgt/updateCustomerGroupInfo`,this.customerGroupFb.value).subscribe((res)=>{
+      this.http.put<ResponsObject>(`${environment.apiUrl}cus_group_mgt/updateCustomerGroupInfo`, this.customerGroupFb.value).subscribe((res) => {
           console.log(res);
 
           if ( res.statusCode === 400) {
 
-            this.notification.create('error','*** New Customer Group ***',res.status);
+            this.notification.create('error', '*** New Customer Group ***', res.status);
 
           } else if ( res.statusCode === 500) {
 
-            this.notification.create('error','*** New Customer Group ***',res.status);
+            this.notification.create('error', '*** New Customer Group ***', res.status);
 
           } else if ( res.statusCode === 200) {
-            this.notification.create('success','*** New Customer Group ***','Sucessfully Updated  Customer Group !');
+            this.notification.create('success', '*** New Customer Group ***', 'Sucessfully Updated  Customer Group !');
             this.clear();
           }
 
@@ -116,23 +116,23 @@ export class CustomerGroupPageComponent implements OnInit {
     this.customerGroupFb.reset();
     this.loadGroupNo();
     this. getAllCustomerGroups();
-    this.saveButtonText ='Save';
+    this.saveButtonText = 'Save';
 
   }
 
   getAllCustomerGroups(){
 
-    this.http.get<any>(environment.apiUrl + 'cus_group_mgt/getAllCustomerGroups').subscribe((res)=>{
-      console.log(' xxxx---> ',res);
-      this.getAllCusGroup =res;
+    this.http.get<any>(environment.apiUrl + 'cus_group_mgt/getAllCustomerGroups').subscribe((res) => {
+      console.log(' xxxx---> ', res);
+      this.getAllCusGroup = res;
 
    });
 
   }
 
-  getRowData(data:any){
+  getRowData(data: any){
 
-    this.saveButtonText='Update';
+    this.saveButtonText = 'Update';
     console.log(data);
 
     this.customerGroupFb.controls.customerGroupNo.setValue(data.customerGroupNo);
--- src/app/pages/customers-mgt/customer-mgt-page/customer-mgt-page.component.ts
+++ src/app/pages/customers-mgt/customer-mgt-page/customer-mgt-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { UniqueNumber } from 'src/app/shard/UniqueNumber';
+import { ResponsObject } from 'src/app/shard/ReponsObject';
 
 @Component({
   selector: 'app-customer-mgt-page',
@@ -8,9 +13,47 @@ import { Router } from '@angular/router';
 })
 export class CustomerMgtPageComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  customersFb:FormGroup;
+  date:Date;
+  customerGroup:any;
+  provinceList:any;
+  customerList:any;
+  listOfCusGroup:any;
+  constructor(private router:Router,private http:HttpClient,private fb:FormBuilder) {
+    // this.getCustomerRegNumber();
+   }
 
   ngOnInit() {
+
+    this.customersFb =this.fb.group({
+      customerSeq:[null],
+      customerReqNo:[null,[Validators.maxLength(20),Validators.required]],
+      nameWithInitials:[null,[Validators.maxLength(140),Validators.required]],
+      fullName:[null,[Validators.maxLength(140),Validators.required]],
+      address:[null,[Validators.maxLength(100),Validators.required]],
+      address2:[null,[Validators.maxLength(100)]],
+      email:[null,[Validators.maxLength(70),Validators.required]],
+      nic:[null,[Validators.maxLength(12),Validators.required]],
+      registeredDate:[null,[Validators.required]],
+      mobileNo:[null,[Validators.maxLength(10),Validators.required]],
+      teleNo:[null,[Validators.maxLength(10),Validators.required]],
+      noOfFamilyMembers:[null,[Validators.maxLength(2),Validators.required]],
+      province:[null,[Validators.maxLength(20),Validators.required]],
+      status:[null,[Validators.maxLength(1),Validators.required]],
+      createdBy:[null],
+      updatedBy:[null],
+      createdDate:[null],
+      UpdatedDate:[null],
+      image:[null],
+      groupId:[null]
+
+
+
+    });
+
+    this.getCustomerRegNumber();
+    this.getAllActiveCustomerGroups();
+
   }
 
   backToHome(){
@@ -23,5 +66,27 @@ export class CustomerMgtPageComponent implements OnInit {
     this.router.navigate(['/home-page'])
 
   }
+
+  // -------------------------------------------------------------
+
+  getCustomerRegNumber() {
+
+    this.http.get<ResponsObject>(environment.apiUrl + 'numbers/getNumberById/' + UniqueNumber.CUSTOMER).subscribe((res) => {
+      this.customersFb.controls.customerReqNo.setValue(res.status);
+    });
+
+  }
+
+
+  getAllActiveCustomerGroups(){
+
+    this.http.get<ResponsObject>(environment.apiUrl + 'cus_group_mgt/getAllCustomerGroups/').subscribe((res) => {
+      console.log(' xxxx---> ', res);
+      this.listOfCusGroup =res;
+
+    });
+
+  }
+
 
 }
